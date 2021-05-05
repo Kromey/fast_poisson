122c62ecb78a88598a387493d4c66c9033cb29da
Travis Veazey <travisvz@gmail.com>
2021-05-05 15:19:32 -0800

    Refactor PoissonIter into iter::Iter

---
+++ src/iter/tests.rs
@@ -0,0 +1,264 @@
+// Copyright 2021 Travis Veazey
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// https://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// https://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
+use super::*;
+use crate::{Poisson2D, Poisson3D};
+
+#[test]
+fn cell_size() {
+    let poisson1 = Poisson::<1>::new().with_dimensions([1.0], 0.1).iter();
+    let poisson2 = Poisson::<2>::new().with_dimensions([1.0; 2], 0.1).iter();
+    let poisson3 = Poisson::<3>::new().with_dimensions([1.0; 3], 0.1).iter();
+    let poisson4 = Poisson::<4>::new().with_dimensions([1.0; 4], 0.1).iter();
+
+    assert_eq!(poisson1.cell_size, 0.1 / Float::from(1.0).sqrt());
+    assert_eq!(poisson2.cell_size, 0.1 / Float::from(2.0).sqrt());
+    assert_eq!(poisson3.cell_size, 0.1 / Float::from(3.0).sqrt());
+    assert_eq!(poisson4.cell_size, 0.1 / Float::from(4.0).sqrt());
+}
+
+#[test]
+fn n_dimensional_grid_size() {
+    for n in 1..=3 {
+        let mut poisson0 = Poisson::<0>::new();
+        let mut poisson1 = Poisson::<1>::new();
+        let mut poisson2 = Poisson::<2>::new();
+        let mut poisson3 = Poisson::<3>::new();
+        let mut poisson4 = Poisson::<4>::new();
+
+        poisson0.dimensions = [];
+        poisson1.dimensions = [n as Float];
+        poisson2.dimensions = [n as Float; 2];
+        poisson3.dimensions = [n as Float; 3];
+        poisson4.dimensions = [n as Float; 4];
+
+        let iter0 = poisson0.iter();
+        let iter1 = poisson1.iter();
+        let iter2 = poisson2.iter();
+        let iter3 = poisson3.iter();
+        let iter4 = poisson4.iter();
+
+        assert_eq!(iter0.grid.len(), 1);
+        assert_eq!(
+            iter1.grid.len(),
+            (n as Float / iter1.cell_size).ceil() as usize
+        );
+        assert_eq!(
+            iter2.grid.len(),
+            ((n as Float / iter2.cell_size).ceil() as usize).pow(2)
+        );
+        assert_eq!(
+            iter3.grid.len(),
+            ((n as Float / iter3.cell_size).ceil() as usize).pow(3)
+        );
+        assert_eq!(
+            iter4.grid.len(),
+            ((n as Float / iter4.cell_size).ceil() as usize).pow(4)
+        );
+    }
+}
+
+#[test]
+fn n_dimensional_cell_to_idx() {
+    let poisson = Poisson::<3> {
+        dimensions: [3., 3., 3.],
+        ..Default::default()
+    };
+    let mut iter = poisson.iter();
+    // Coerce cell_size to more easily test cell_to_idx function
+    iter.cell_size = 1.;
+
+    assert_eq!(iter.cell_to_idx([0, 0, 0]), 0);
+    assert_eq!(iter.cell_to_idx([1, 1, 1]), 13);
+    assert_eq!(iter.cell_to_idx([1, 2, 1]), 16);
+    assert_eq!(iter.cell_to_idx([2, 1, 1]), 22);
+
+    let poisson = Poisson::<2> {
+        dimensions: [3., 3.],
+        ..Default::default()
+    };
+    let mut iter = poisson.iter();
+    // Coerce cell_size to more easily test cell_to_idx function
+    iter.cell_size = 1.;
+
+    assert_eq!(iter.cell_to_idx([0, 0]), 0);
+    assert_eq!(iter.cell_to_idx([1, 1]), 4);
+    assert_eq!(iter.cell_to_idx([1, 2]), 5);
+    assert_eq!(iter.cell_to_idx([2, 1]), 7);
+}
+
+#[test]
+fn n_dimensional_point_to_cell() {
+    let poisson = Poisson::<3> {
+        dimensions: [3., 3., 3.],
+        ..Default::default()
+    };
+    let mut iter = poisson.iter();
+    // Coerce cell_size to more easily test point_to_cell function
+    iter.cell_size = 2.;
+
+    assert_eq!(iter.point_to_cell([0., 0., 0.]), [0, 0, 0]);
+    assert_eq!(iter.point_to_cell([1., 1., 1.]), [0, 0, 0]);
+    assert_eq!(iter.point_to_cell([1., 2., 1.]), [0, 1, 0]);
+    assert_eq!(iter.point_to_cell([2., 3., 1.]), [1, 1, 0]);
+
+    let poisson = Poisson::<2> {
+        dimensions: [3., 3.],
+        ..Default::default()
+    };
+    let mut iter = poisson.iter();
+    // Coerce cell_size to more easily test point_to_cell function
+    iter.cell_size = 2.;
+
+    assert_eq!(iter.point_to_cell([0., 0.]), [0, 0]);
+    assert_eq!(iter.point_to_cell([1., 1.]), [0, 0]);
+    assert_eq!(iter.point_to_cell([1., 2.]), [0, 1]);
+    assert_eq!(iter.point_to_cell([2., 3.]), [1, 1]);
+}
+
+#[test]
+fn sample_to_grid() {
+    let iter = Poisson::<2>::new().iter();
+
+    for &point in &[[0.0, 0.0], [0.5, 0.5], [1.0, 1.0]] {
+        let idx = iter.point_to_idx(point);
+
+        // Trying to access this will panic if it's out of bound in any way
+        // TODO: Should do more robust testing of the results
+        let _ = iter.grid[idx];
+    }
+}
+
+#[test]
+fn adding_points() {
+    let mut iter = Poisson::<2>::new().iter();
+    let point = [0.5, 0.5];
+
+    iter.add_point(point);
+
+    assert!(iter.active.contains(&point));
+
+    let idx = iter.point_to_idx(point);
+    assert_eq!(iter.grid[idx], Some(point));
+}
+
+#[test]
+fn point_generation_lies_within_radius() {
+    let mut iter = Poisson2D::new().iter();
+    let initial = [0.5; 2];
+
+    for _ in 0..50 {
+        let point = iter.generate_random_point(initial);
+
+        let r = point
+            .iter()
+            .zip(initial.iter())
+            .map(|(a, b)| (a - b).powi(2))
+            .sum::<Float>()
+            .sqrt();
+
+        assert!(r > iter.distribution.radius);
+        assert!(r < iter.distribution.radius * 2.);
+    }
+
+    let mut iter = Poisson3D::new().iter();
+    let initial = [0.5; 3];
+
+    for _ in 0..50 {
+        let point = iter.generate_random_point(initial);
+
+        let r = point
+            .iter()
+            .zip(initial.iter())
+            .map(|(a, b)| (a - b).powi(2))
+            .sum::<Float>()
+            .sqrt();
+
+        assert!(r > iter.distribution.radius);
+        assert!(r < iter.distribution.radius * 2.);
+    }
+}
+
+#[test]
+fn generate_random_point() {
+    let mut iter = Poisson::<2>::new().iter();
+
+    for _ in 0..100 {
+        let [x, y] = iter.generate_random_point([0.5, 0.5]);
+
+        assert!(0.0 <= x);
+        assert!(1.0 > x);
+        assert!(0.0 <= y);
+        assert!(1.0 > y);
+    }
+}
+
+#[test]
+fn in_space() {
+    let iter = Poisson::<2>::new().iter();
+
+    // Affirmative tests
+    assert!(iter.in_space([0.0, 0.0]));
+    assert!(iter.in_space([0.5, 0.5]));
+
+    // Negative tests
+    assert!(!iter.in_space([1.0, 1.0]));
+    assert!(!iter.in_space([1.0, 2.0]));
+    assert!(!iter.in_space([-0.1, 0.0]));
+}
+
+#[test]
+fn empty_grid_has_no_neighbors() {
+    let mut iter = Poisson::<2>::new().iter();
+    // Flush the grid
+    iter.grid = vec![None; iter.grid.len()];
+
+    assert!(!iter.in_neighborhood([0.1, 0.1]));
+    assert!(!iter.in_neighborhood([0.2, 0.2]));
+    assert!(!iter.in_neighborhood([1.1, 1.1])); // Out of bounds by definition has no neighbors
+}
+
+#[test]
+fn distant_point_has_no_neighbors() {
+    let mut iter = Poisson::<2>::new().iter();
+    // Flush the grid
+    iter.grid = vec![None; iter.grid.len()];
+
+    // Add test point
+    iter.add_point([0.9, 0.9]);
+
+    assert!(!iter.in_neighborhood([0.1, 0.1]));
+    assert!(!iter.in_neighborhood([0.2, 0.2]));
+    assert!(!iter.in_neighborhood([0.8, 0.8]));
+}
+
+#[test]
+fn point_has_neighbors() {
+    let mut iter = Poisson::<2>::new().iter();
+    // Flush the grid
+    iter.grid = vec![None; iter.grid.len()];
+
+    // Add test point
+    iter.add_point([0.2, 0.2]);
+
+    assert!(iter.in_neighborhood([0.2, 0.2])); // Same point is a neighbor
+    assert!(iter.in_neighborhood([0.2005, 0.2])); // Close point is a neighbor
+}
+
+#[test]
+fn out_of_bounds_point_is_not_neighbor() {
+    let mut iter = Poisson::<2>::new().iter();
+    // Flush the grid
+    iter.grid = vec![None; iter.grid.len()];
+
+    // Enlarge radius
+    iter.distribution.radius = 0.5;
+    // Add test point near perimeter
+    iter.add_point([0.9, 0.9]);
+
+    assert!(!iter.in_neighborhood([1.1, 1.1])); // Out of bounds by definition has no neighbors
+}
--- src/lib.rs
+++ src/lib.rs
@@ -153,9 +153,8 @@
 #[cfg(test)]
 mod tests;
 
-use rand::prelude::*;
-use rand_distr::StandardNormal;
-use std::iter::FusedIterator;
+mod iter;
+pub use iter::{Iter, Point};
 
 /// [`Poisson`] disk distribution in 2 dimensions
 pub type Poisson2D = Poisson<2>;
@@ -280,8 +279,8 @@ impl<const N: usize> Poisson<N> {
     /// }
     /// ```
     #[must_use]
-    pub fn iter(&self) -> PoissonIter<N> {
-        PoissonIter::new(self.clone())
+    pub fn iter(&self) -> Iter<N> {
+        Iter::new(self.clone())
     }
 
     /// Generate the points in this Poisson distribution, collected into a [`Vec`](std::vec::Vec).
@@ -382,16 +381,16 @@ impl<const N: usize> Default for Poisson<N> {
 
 impl<const N: usize> IntoIterator for Poisson<N> {
     type Item = Point<N>;
-    type IntoIter = PoissonIter<N>;
+    type IntoIter = Iter<N>;
 
     fn into_iter(self) -> Self::IntoIter {
-        PoissonIter::new(self)
+        Iter::new(self)
     }
 }
 
 impl<const N: usize> IntoIterator for &Poisson<N> {
     type Item = Point<N>;
-    type IntoIter = PoissonIter<N>;
+    type IntoIter = Iter<N>;
 
     fn into_iter(self) -> Self::IntoIter {
         self.iter()
@@ -407,226 +406,6 @@ where
         poisson.to_vec()
     }
 }
-
-/// A Point is simply an array of Float values
-type Point<const N: usize> = [Float; N];
-
-/// A Cell is the grid coordinates containing a given point
-type Cell<const N: usize> = [isize; N];
-
-#[cfg(not(feature = "small_rng"))]
-type Rand = rand_xoshiro::Xoshiro256StarStar;
-#[cfg(feature = "small_rng")]
-type Rand = rand_xoshiro::Xoshiro128StarStar;
-
-/// An iterator over the points in the Poisson disk distribution
-pub struct PoissonIter<const N: usize> {
-    /// The distribution from which this iterator was built
-    distribution: Poisson<N>,
-    /// The RNG
-    rng: Rand,
-    /// The size of each cell in the grid
-    cell_size: Float,
-    /// The grid stores spatially-oriented samples for fast checking of neighboring sample points
-    grid: Vec<Option<Point<N>>>,
-    /// A list of valid points that we have not yet visited
-    active: Vec<Point<N>>,
-}
-
-impl<const N: usize> PoissonIter<N> {
-    /// Create an iterator over the specified distribution
-    fn new(distribution: Poisson<N>) -> Self {
-        // We maintain a grid of our samples for faster radius checking
-        let cell_size = distribution.radius / (N as Float).sqrt();
-
-        // If we were not given a seed, generate one non-deterministically
-        let mut rng = match distribution.seed {
-            None => Rand::from_entropy(),
-            Some(seed) => Rand::seed_from_u64(seed),
-        };
-
-        // Calculate the amount of storage we'll need for our n-dimensional grid, which is stored
-        // as a single-dimensional array.
-        let grid_size: usize = distribution
-            .dimensions
-            .iter()
-            .map(|n| (n / cell_size).ceil() as usize)
-            .product();
-
-        // We have to generate an initial point, just to ensure we've got *something* in the active list
-        let mut first_point = [0.0; N];
-        for (i, dim) in first_point.iter_mut().zip(distribution.dimensions.iter()) {
-            *i = rng.gen::<Float>() * dim;
-        }
-
-        let mut iter = PoissonIter {
-            distribution,
-            rng,
-            cell_size,
-            grid: vec![None; grid_size],
-            active: Vec::new(),
-        };
-        // Don't forget to add our initial point
-        iter.add_point(first_point);
-
-        iter
-    }
-
-    /// Add a point to our pattern
-    fn add_point(&mut self, point: Point<N>) {
-        // Add it to the active list
-        self.active.push(point);
-
-        // Now stash this point in our grid
-        let idx = self.point_to_idx(point);
-        self.grid[idx] = Some(point);
-    }
-
-    /// Convert a point into grid cell coordinates
-    fn point_to_cell(&self, point: Point<N>) -> Cell<N> {
-        let mut cell = [0_isize; N];
-
-        for i in 0..N {
-            cell[i] = (point[i] / self.cell_size) as isize;
-        }
-
-        cell
-    }
-
-    /// Convert a cell into a grid vector index
-    fn cell_to_idx(&self, cell: Cell<N>) -> usize {
-        cell.iter()
-            .zip(self.distribution.dimensions.iter())
-            .fold(0, |acc, (pn, dn)| {
-                acc * (dn / self.cell_size) as usize + *pn as usize
-            })
-    }
-
-    /// Convert a point into a grid vector index
-    fn point_to_idx(&self, point: Point<N>) -> usize {
-        self.cell_to_idx(self.point_to_cell(point))
-    }
-
-    /// Generate a random point between `radius` and `2 * radius` away from the given point
-    fn generate_random_point(&mut self, around: Point<N>) -> Point<N> {
-        // Pick a random distance away from our point
-        let dist = self.distribution.radius * (1.0 + self.rng.gen::<Float>());
-
-        // Generate a randomly distributed vector
-        let mut vector: [Float; N] = [0.0; N];
-        for i in vector.iter_mut() {
-            *i = self.rng.sample(StandardNormal);
-        }
-        // Now find this new vector's magnitude
-        let mag = vector.iter().map(|&x| x.powi(2)).sum::<Float>().sqrt();
-
-        // Dividing each of the vector's components by `mag` will produce a unit vector; then by
-        // multiplying each component by `dist`, we'll have a vector pointing `dist` away from the
-        // origin. If we then add each of those components to our point, we'll have effectively
-        // translated our point by `dist` in a randomly chosen direction.
-        // Conveniently, we can do all of this in just one step!
-        let mut point = [0.0; N];
-        let translate = dist / mag; // compute this just once!
-        for i in 0..N {
-            point[i] = around[i] + vector[i] * translate;
-        }
-
-        point
-    }
-
-    /// Returns true if the point is within the bounds of our space.
-    ///
-    /// This is true if 0 ≤ point[i] < dimensions[i]
-    fn in_space(&self, point: Point<N>) -> bool {
-        point
-            .iter()
-            .zip(self.distribution.dimensions.iter())
-            .all(|(p, d)| *p >= 0. && p < d)
-    }
-
-    /// Returns true if the cell is within the bounds of our grid.
-    ///
-    /// This is true if 0 ≤ `cell[i]` ≤ `ceiling(space[i] / cell_size)`
-    fn in_grid(&self, cell: Cell<N>) -> bool {
-        cell.iter()
-            .zip(self.distribution.dimensions.iter())
-            .all(|(c, d)| *c >= 0 && *c < (*d / self.cell_size).ceil() as isize)
-    }
-
-    /// Returns true if there is at least one other sample point within `radius` of this point
-    fn in_neighborhood(&self, point: Point<N>) -> bool {
-        let cell = self.point_to_cell(point);
-
-        // We'll compare to distance squared, so we can skip the square root operation for better performance
-        let r_squared = self.distribution.radius.powi(2);
-
-        for mut carry in 0.. {
-            let mut neighbor = cell;
-
-            // We can add our current iteration count to visit each neighbor cell
-            for i in (&mut neighbor).iter_mut() {
-                // We clamp our addition to the range [-2, 2] for each cell
-                *i += carry % 5 - 2;
-                // Since we modulo by 5 to get the right range, integer division by 5 "advances" us
-                carry /= 5;
-            }
-
-            if carry > 0 {
-                // If we've "overflowed" then we've already tested every neighbor cell
-                return false;
-            }
-            if !self.in_grid(neighbor) {
-                // Skip anything beyond the bounds of our grid
-                continue;
-            }
-
-            if let Some(point2) = self.grid[self.cell_to_idx(neighbor)] {
-                let neighbor_dist_squared = point
-                    .iter()
-                    .zip(point2.iter())
-                    .map(|(a, b)| (a - b).powi(2))
-                    .sum::<Float>();
-
-                if neighbor_dist_squared < r_squared {
-                    return true;
-                }
-            }
-        }
-
-        // Rust can't tell the previous loop will always reach one of the `return` statements...
-        false
-    }
-}
-
-impl<const N: usize> Iterator for PoissonIter<N> {
-    type Item = Point<N>;
-
-    fn next(&mut self) -> Option<Point<N>> {
-        while !self.active.is_empty() {
-            let i = self.rng.gen_range(0..self.active.len());
-
-            for _ in 0..self.distribution.num_samples {
-                // Generate up to `num_samples` random points between radius and 2*radius from the current point
-                let point = self.generate_random_point(self.active[i]);
-
-                // Ensure we've picked a point inside the bounds of our rectangle, and more than `radius`
-                // distance from any other sampled point
-                if self.in_space(point) && !self.in_neighborhood(point) {
-                    // We've got a good one!
-                    self.add_point(point);
-
-                    return Some(point);
-                }
-            }
-
-            self.active.swap_remove(i);
-        }
-
-        None
-    }
-}
-
-impl<const N: usize> FusedIterator for PoissonIter<N> {}
 
 // Hacky way to include README in doc-tests, but works until #[doc(include...)] is stabilized
 // https://github.com/rust-lang/cargo/issues/383#issuecomment-720873790
