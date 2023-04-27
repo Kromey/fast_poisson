07ec955d21ddb461dccbb231770a5035d3c77322
Travis Veazey <travisvz@gmail.com>
2023-04-27 15:28:16 -0800

    New test to check 1 million seeds for "closeness"

--- tests/closeness.rs
+++ tests/closeness.rs
@@ -1,4 +1,5 @@
 use fast_poisson::Poisson2D;
+use rayon::prelude::{IntoParallelIterator, ParallelIterator};
 
 /// Ensure points remain at minimum radius apart
 ///
@@ -19,6 +20,31 @@ fn closeness() {
             assert!(5.0 <= distance(points[i], points[j]));
         }
     }
+}
+
+/// Thoroughly ensure points remain at minimum radius apart
+///
+/// Ref #33
+#[test]
+#[ignore = "This test checks 1 million seeds in parallel to ensure minimum distance guarantees are maintained"]
+fn closeness_thorough() {
+    (0..1_000_000).into_par_iter().for_each(|seed| {
+        let points = Poisson2D::new()
+            .with_dimensions([30.0, 20.0], 5.0)
+            .with_seed(seed)
+            .generate();
+
+        if !points.is_empty() {
+            // FIXME: Need a test to handle empty results; see #34
+            // Test every point against every other point
+            for i in 0..(points.len() - 1) {
+                // Only need to test points later in the list, since we've already tested i against earlier points
+                for j in (i+1)..points.len() {
+                    assert!(5.0 <= distance(points[i], points[j]));
+                }
+            }
+        }
+    });
 }
 
 fn distance(p1: [f64; 2], p2: [f64; 2]) -> f64 {
