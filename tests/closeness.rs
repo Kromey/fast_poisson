5dc0f5e44a6f462519d861be1ef6a7bd1bbaae24
Travis Veazey <travisvz@gmail.com>
2023-04-27 17:05:06 -0800

    Remove rayon and parallel tests
    Not supported at our MSRV of 1.51; may bump MSRV or otherwise find a way to ensure rayon is only included when needed

--- tests/closeness.rs
+++ tests/closeness.rs
@@ -1,5 +1,4 @@
 use fast_poisson::Poisson2D;
-use rayon::prelude::{IntoParallelIterator, ParallelIterator};
 
 #[cfg(not(feature = "single_precision"))]
 type Float = f64;
@@ -25,31 +24,6 @@ fn closeness() {
             assert!(5.0 <= distance(points[i], points[j]));
         }
     }
-}
-
-/// Thoroughly ensure points remain at minimum radius apart
-///
-/// Ref #33
-#[test]
-#[ignore = "This test checks 1 million seeds in parallel to ensure minimum distance guarantees are maintained"]
-fn closeness_thorough() {
-    (0..1_000_000).into_par_iter().for_each(|seed| {
-        let points = Poisson2D::new()
-            .with_dimensions([30.0, 20.0], 5.0)
-            .with_seed(seed)
-            .generate();
-
-        if !points.is_empty() {
-            // FIXME: Need a test to handle empty results; see #34
-            // Test every point against every other point
-            for i in 0..(points.len() - 1) {
-                // Only need to test points later in the list, since we've already tested i against earlier points
-                for j in (i + 1)..points.len() {
-                    assert!(5.0 <= distance(points[i], points[j]));
-                }
-            }
-        }
-    });
 }
 
 fn distance(p1: [Float; 2], p2: [Float; 2]) -> Float {
--- tests/emptiness.rs
+++ tests/emptiness.rs
@@ -1,5 +1,4 @@
 use fast_poisson::Poisson2D;
-use rayon::prelude::{IntoParallelIterator, ParallelIterator};
 
 /// Ensure points remain at minimum radius apart
 ///
@@ -19,25 +18,4 @@ fn emptiness() {
             seed
         );
     }
-}
-
-/// Thoroughly ensure points remain at minimum radius apart
-///
-/// Ref #33
-#[test]
-#[ignore = "This test checks 1 million seeds in parallel to ensure generated points are not empty"]
-fn emptiness_thorough() {
-    (0..1_000_000).into_par_iter().for_each(|seed| {
-        let points = Poisson2D::new()
-            .with_dimensions([30.0, 20.0], 5.0)
-            .with_seed(seed)
-            .generate();
-
-        // Verify we actually have points
-        assert!(
-            !points.is_empty(),
-            "Seed {} produced an empty set of points",
-            seed
-        );
-    });
 }
