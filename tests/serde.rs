fc520ece7c8831e0f8a77e466088e0a4e4672677
Travis Veazey <travisvz@gmail.com>
2023-04-28 13:03:57 -0800

    Fix serde test for updated builder pattern

--- tests/serde.rs
+++ tests/serde.rs
@@ -6,8 +6,7 @@ use serde_json;
 #[test]
 fn serialize_and_deserialize() {
     // Be sure to set a seed so we can assert equality
-    let mut poisson = Poisson2D::new();
-    poisson.with_seed(1337);
+    let poisson = Poisson2D::new().with_seed(1337);
 
     let json = serde_json::to_string(&poisson).unwrap();
     let decoded = serde_json::from_str(&json).unwrap();
